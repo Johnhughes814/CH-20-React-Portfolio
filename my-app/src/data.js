4fa376e8a1a69cfb2d666ca7317ff267d9ca2f13
Johnhughes814 <8hughes.john@gmail.com>
2022-04-20 13:30:43 -0500

    first project entered

---
+++ my-app/src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+      id: 1,
+      title: "Work Buddies",
+      img: "https://raw.githubusercontent.com/Johnhughes814/CH-10-Work-Buddies/main/workBuddies.png",
+      gitHubLink: "https://github.com/Johnhughes814/CH-10-Work-Buddies",
+      projectLink: "https://www.youtube.com/watch?v=RA0fSl1yqOc",
+      description: "Work Buddies is a command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.",
+      tech: "html, css, javascript, bash, CLI, inquirer, jest"
+    }
