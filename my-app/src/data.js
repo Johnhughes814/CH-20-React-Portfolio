fbfe935c6286571b15576ba4991e1bbba1d0e2b6
Johnhughes814 <8hughes.john@gmail.com>
2022-04-20 13:49:44 -0500

    another project entered

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -7,4 +7,14 @@ export const projects = [
       projectLink: "https://www.youtube.com/watch?v=RA0fSl1yqOc",
       description: "Work Buddies is a command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.",
       tech: "html, css, javascript, bash, CLI, inquirer, jest"
+    },
+    {
+      id: 2,
+      title: "BeersandBands",
+      img: "https://cdn.pixabay.com/photo/2017/03/13/23/31/icon-2141485_960_720.png",
+      gitHubLink: "https://github.com/sarahwylie/BeersandBands",
+      projectLink: "https://sarahwylie.github.io/BeersandBands/",
+      description: "A one-stop site for your night out! Find a local brewpub and your favorite band or concert venue. Search by city and date to plan one or several nights of drunken moshing, chill jamming, or groovy grinding.",
+      tech: "html, css, javascript, JQUERY, ESLINT"
+  
     }]
