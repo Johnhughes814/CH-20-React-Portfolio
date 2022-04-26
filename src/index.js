274758d21eeea32e36decabff39539871b43f6df
Johnhughes814 <8hughes.john@gmail.com>
2022-04-26 11:31:52 -0500

    setup index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import { ThemeProvider } from './context';
+
+ReactDom.render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <App/ >
+        </ThemeProvider>
+    </React.StrictMode>
+);
