955835e6ecd3f0a57fbed0adf8c2d7f3f25ed14e
Johnhughes814 <8hughes.john@gmail.com>
2022-04-27 05:37:53 -0500

    made some changes

--- src/data.js
+++ src/data.js
@@ -17,4 +17,44 @@ export const projects = [
       description: "A one-stop site for your night out! Find a local brewpub and your favorite band or concert venue. Search by city and date to plan one or several nights of drunken moshing, chill jamming, or groovy grinding.",
       tech: "html, css, javascript, JQUERY, ESLINT"
   
+    },
+    {
+      id: 3,
+      title: "Weather Dashboard",
+      img: "https://cdn.pixabay.com/photo/2019/05/19/23/47/clouds-4215608_1280.jpg",
+      gitHubLink: "https://github.com/Johnhughes814/CH-6-Weather-Dashboard",
+      projectLink: "https://johnhughes814.github.io/CH-6-Weather-Dashboard/",
+      description: "A website to help you keep track of the weather in any given city!",
+      tech: "html, css, javascript"
+  
+    },
+    {
+      id: 4,
+      title: "Markdown Maker",
+      img: "",
+      gitHubLink: "https://github.com/Johnhughes814/CH-9-Markdown-Maker",
+      projectLink: "https://www.youtube.com/watch?v=9CXSRNF63Ho",
+      description: "a command-line application that dynamically generates a professional README.md file from a user's input",
+      tech: ""
+  
+    },
+    {
+      id: 5,
+      title: "Note Taker",
+      img: "https://raw.githubusercontent.com/Johnhughes814/CH-11-Notetaker/main/Notetaker.png",
+      gitHubLink: "https://github.com/Johnhughes814/CH-11-Notetaker",
+      projectLink: "https://johnhughes814.github.io/CH-11-Notetaker/",
+      description: "A simple HTML, CSS, and JS website that uses local storage to save entries in your cache. User has the option to delete notes as well!",
+      tech: "HTML, CSS, JS"
+  
+    },
+    {
+      id: 6,
+      title: "Model View Controller",
+      img: "",
+      gitHubLink: "https://github.com/Johnhughes814/CH-14-MVC",
+      projectLink: "",
+      description: "A blog that allows users to login, submit posts, comment on posts, and delete posts. Data is stored in a DB so all comments and are accessible to all client.",
+      tech: ""
+  
     }]
