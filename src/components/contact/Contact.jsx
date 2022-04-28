1272efc8e0f872165b82dccf4fb9b5f1e7e53282
Johnhughes814 <8hughes.john@gmail.com>
2022-04-28 13:49:56 -0500

    test

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
             </div>
             <div className="c_info-item">
               <img className="c_icon" src={Email} alt="" />
-              8hughes.john@gmail.com 
+              8hughes.john@gmail.com
             </div>
             <div className="c_info-item">
               <img className="c_icon" src={Address} alt="" />
@@ -28,7 +28,6 @@ const Contact = () => {
           </div>
         </div>
         <div className="c_right">
-         FILLER INFO / GRAPHICS / ???
         </div>
       </div>
     </div>
