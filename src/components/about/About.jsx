20b27c5c788200e72ee455267fa25c94023e116c
Johnhughes814 <8hughes.john@gmail.com>
2022-04-28 05:08:29 -0500

    about

---
+++ src/components/about/About.jsx
@@ -0,0 +1,38 @@
+import "./about.css";
+import PlaceholderImage from "../../img/guy.png";
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a_left">
+        <div className="a_card bg"></div>
+        <div className="a_card">
+          <img src="https://cdn.pixabay.com/photo/2016/10/02/19/51/chip-1710300_960_720.png" alt="" className="a_img" />
+        </div>
+      </div>
+      <div className="a_right">
+        <h1 className="a_title">About Me</h1>
+        <p className="a_sub">
+          One Sentence here perhaps this long this long this long this long this long this long this long this long this long this long.
+        </p>
+        <p className="a_desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+        <div className="a_sub">
+          <img src={PlaceholderImage} alt="" className="a_sub-img" />
+          <div className="a_sub-texts">
+            <h4 className="a_sub-title">Skills</h4>
+            <p className="a_sub-desc">
+              IMAGES OF SKILLS? LIST OF SKILLS?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
