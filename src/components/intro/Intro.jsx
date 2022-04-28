a08a021bb3a2a9b878f8200886baffcfd58bf29c
Johnhughes814 <8hughes.john@gmail.com>
2022-04-28 04:34:52 -0500

    intro

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,36 @@
+import "./intro.css";
+import Me from "../../img/thatGuy.png";
+
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i_left">
+        <div className="i_left-wrapper">
+          <h2 className="i_intro">Hello, My name is</h2>
+          <h1 className="i_name">John Hughes</h1>
+          <div className="i_title">
+            <div className="i_title-wrapper">
+              <div className="i_title-item">Web Developer</div>
+              <div className="i_title-item">UI/UX Designer</div>
+              <div className="i_title-item">YOUR NEXT EMPLOYEE</div>
+              <div className="i_title-item">WORKAHOLIC</div>
+              <div className="i_title-item">there for YOU</div>
+            </div>
+          </div>
+          <p className="i_desc">
+            1 - 2 sentences about this length about this length about this length 
+            about this length about this length about this length about this length
+            about this length about this length about this length.
+          </p>
+        </div>
+      
+      </div>
+      <div className="i_right">
+        <div className="i_bg"></div>
+        <img src={Me} alt="" className="i_img" />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
